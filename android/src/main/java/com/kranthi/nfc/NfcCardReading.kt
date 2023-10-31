c333ca55b35df641cf75adadac8417a2824ae071
Burra Kranthi Kiran <developer.startoonlabs@gmail.com>
2023-10-31 17:59:51 +0530

    first commit

---
+++ android/src/main/java/com/kranthi/nfc/NfcCardReading.kt
@@ -0,0 +1,10 @@
+package com.kranthi.nfc
+
+import android.util.Log
+
+class NfcCardReading {
+    fun myFunction() {
+        // Function logic goes here
+        Log.e("9999999999", "Testing")
+    }
+}
